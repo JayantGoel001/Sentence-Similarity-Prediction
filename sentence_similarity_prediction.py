29285d3524e98773c64e19c12881453841136701
Jayant Goel <jgoel92@gmail.com>
2021-12-01 14:07:58 +0530

    Initial Commit

--- sentence_similarity_prediction.py
+++ sentence_similarity_prediction.py
@@ -18,7 +18,8 @@ subprocess.run(["apt-get","install", "-y", "openjdk-8-jdk-headless", "-qq"])
 print("USER",os.listdir("/usr"))
 print("LIB",os.listdir("/usr/lib"))
 for p in os.listdir("/usr/lib"):
-    print(os.listdir("/usr/lib/" + p))
+    if os.isdir("/usr/lib/" + p):
+        print(os.listdir("/usr/lib/" + p))
 
 os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
 os.environ["PATH"] = os.environ["JAVA_HOME"] + "/bin:" + os.environ["PATH"]
