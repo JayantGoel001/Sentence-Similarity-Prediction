814850f33e2b0ba1935fe91a18cd62cc7760d516
Jayant Goel <jgoel92@gmail.com>
2021-12-01 14:55:17 +0530

    Initial Commit

--- sentence_similarity_prediction.py
+++ sentence_similarity_prediction.py
@@ -8,12 +8,9 @@ Original file is located at
 """
 
 import os
-import subprocess
 
-path = os.getcwd()
-
-subprocess.run(["sudo","apt-get", "update", "-qq"])
-subprocess.run(["sudo","apt-get","install", "-y", "openjdk-8-jdk-headless", "-qq"])
+os.system("apt-get update -qq")
+os.system("!apt-get install -y openjdk-8-jdk-headless -qq")
 
 os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
 os.environ["PATH"] = os.environ["JAVA_HOME"] + "/bin:" + os.environ["PATH"]
